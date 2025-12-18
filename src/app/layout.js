90c8615e0535839ea0435d4d2635d9f4c055dc3d
Suman-Limbu <sumanscoopie123@gmail.com>
2025-12-18 21:15:09 +0545

    routing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 export const metadata = {
   title: " Dokan",
@@ -9,9 +10,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header>This is header</header>{children}
+        <header>
+          <div className="container mx-auto py-1 px-2">
+            <div className="flex justify-between">
+              <h1 className="font-bold text-2xl">Dokan</h1>
+              <nav className="flex gap-4">
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/products">Products</Link>
+                <Link href="/contact">Contact</Link>
+              </nav>
+            </div>
+          </div>
+        </header>
+        {children}
         <footer>This is footer</footer>
-        </body>
+      </body>
     </html>
   );
 }
