88ca01f8a3b14c46a41fd559848c8adebe4e0b5a
Suman-Limbu <sumanscoopie123@gmail.com>
2025-12-21 23:32:19 +0545

    a

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
-import { config } from './../config/index';
+import Link from "next/link";
+import { config } from "./../config/index";
 
 const Logo = () => {
-    const {appName} = config
+  const { appName } = config;
   return (
     <Link href="/" className="text-xl">
-      <div className="flex items-center justify-start font-semibold hover:font-bold w-40">
+      <div className="flex items-center justify-start font-semibold hover:font-bold w-135">
         <span className="text-secondary">{appName}</span>
       </div>
     </Link>
