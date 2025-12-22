3c53ca92a21164b25dd35247e7e76eae1dbefbf5
Suman-Limbu <sumanscoopie123@gmail.com>
2025-12-22 17:15:40 +0545

    navlink

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,22 +1,17 @@
 import Link from "next/link";
+import navLinks from "./../constants/navlinks";
 const NavMenu = () => {
   return (
     <nav className="hidden md:flex gap-5">
-      <Link href="/" className="text-sm hover:text-primary">
-        Home
-      </Link>
-      <Link href="/products" className="text-sm hover:text-primary">
-        Products
-      </Link>
-      <Link href="/popular-products" className="text-sm hover:text-primary">
-        Popular
-      </Link>
-      <Link href="/categories" className="text-sm hover:text-primary">
-        Categories
-      </Link>
-      <Link href="/testimonials" className="text-sm hover:text-primary">
-        Testimonials
-      </Link>
+      {navLinks.map((navLink, index) => (
+        <Link
+          key={index}
+          href={navLink.route}
+          className="text-sm hover:text-primary"
+        >
+          {navLink.label}
+        </Link>
+      ))}
     </nav>
   );
 };
---
+++ src/constants/navlinks.js
@@ -0,0 +1,16 @@
+const navLinks = [
+  {
+    route: "/",
+    label: "Home",
+  },
+  {
+    route: "/about",
+    label: "About Us",
+  },
+  { route: "/products", label: "Products" },
+  {
+    route: "/contact",
+    label: "contacts",
+  },
+];
+export default navLinks;
