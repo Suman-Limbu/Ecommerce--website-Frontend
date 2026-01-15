32349630f04b3165467da943d24b375ff35acd11
Suman-Limbu <sumanscoopie123@gmail.com>
2026-01-15 18:58:20 +0545

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,16 +24,4 @@ export default function RootLayout({ children }) {
 }
 
 
-function home(){
 
-}
-
-const home=()=>{
-
-}
-
-home();
-const arr1=[1,2,3]
-const arr2=[4,5,6]
-console.log(arr1+arr2)
- [1,2,3,4,5,6]
